f6861dbb236dc396f777e1924ec0b64ff5f0c1cb
Cody-Wise <cody@sharptackmedia.com>
2022-03-10 19:04:05 -0800

    Fixed errors

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 // import functions and grab DOM elements
-const waterFrontContainer = document.getElementById('waterfront-container');
-const skylineContainer = document.getElementById('skyline-container');
-const natureContainer = document.getElementById('nature-container');
-
 const waterFrontImageEl = document.getElementById('waterfront-image');
 const skylineImageEl = document.getElementById('skyline-image');
 const natureImageEl = document.getElementById('nature-image');
 const waterFrontDropdownEl = document.getElementById('waterfront-dropdown');
 const skylineDropdownEl = document.getElementById('skyline-dropdown');
 const natureDropdownEl = document.getElementById('nature-dropdown');
-const sloganEl = document.getElementById('slogan');
 const sloganInputEl = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
 const reportEl = document.getElementById('report');
 const sloganListEl = document.getElementById('slogan-list');
-const welcomeTextContainer = document.getElementById('welcome-text');
 const welcomeTextInput = document.getElementById('welcome-input');
 const welcomeDisplay = document.getElementById('welcome-display');
 
@@ -92,7 +86,7 @@ sloganButton.addEventListener('click', () => {
 
 function displayCountStats(){
 
-    reportEl.textContent = `The waterfront images have been changed ${waterfrontCount} times, the skyline images have been changed ${skylineCount} times and the nature images have been changed ${natureCount} times`
+    reportEl.textContent = `The waterfront images have been changed ${waterfrontCount} times, the skyline images have been changed ${skylineCount} times and the nature images have been changed ${natureCount} times`;
 }
 
 function displaySlogans(){
